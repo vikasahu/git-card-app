5ef7a5d4a76dbebe6bb9dc43753c6e3540e77c80
vikasahu <vikas.sahu@rentomojo.com>
2020-04-10 14:43:05 +0530

    Updates

---
+++ precache-manifest.759b318736a67a7c0e854af4021b319a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefa6fdcfaabc24a8ab1733482d8cb50",
+    "url": "/git-card-app/index.html"
+  },
+  {
+    "revision": "4615c07dc994863fc100",
+    "url": "/git-card-app/static/css/main.57188492.chunk.css"
+  },
+  {
+    "revision": "ec9fb869bb09e0c8ca99",
+    "url": "/git-card-app/static/js/2.bb1c7482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-card-app/static/js/2.bb1c7482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4615c07dc994863fc100",
+    "url": "/git-card-app/static/js/main.d9aaea60.chunk.js"
+  },
+  {
+    "revision": "362e1fce27dc97d94edd",
+    "url": "/git-card-app/static/js/runtime-main.86a25078.js"
+  }
+]);
